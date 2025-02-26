03d041258fb3208c9f09cb2a49eb6fde50b0e7d3
Chen Mor <chen.mor@proqure.io>
2025-02-26 22:53:20 +0800

    bbb

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';   // <-- Import HashRouter
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
