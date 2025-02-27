8af259ade4e2f1070e7a03fd6bcd76a7683d332c
Chen Mor <chen.mor@proqure.io>
2025-02-27 16:51:09 +0800

    Fix GitHub Pages deployment issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';   // <-- Import HashRouter
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -8,8 +7,6 @@ const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
